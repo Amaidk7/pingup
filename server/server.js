50fc8e35f90f7ec36506e74d130d03ad7679613f
Amaidk7 <amaidkhan515@gmail.com>
2026-02-03 22:50:11 +0530

    inngest error fix

--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ import 'dotenv/config'
 import { connect } from 'mongoose'
 import connectDB from './configs/db.js'
 import {inngest,functions} from './inngest/index.js'
+import {serve} from 'inngest/express'
 const app=express();
 
 await connectDB()
