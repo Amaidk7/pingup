f4f6811b24f60dc2b2ee9e777c8ff7530f5133b6
Amaidk7 <amaidkhan515@gmail.com>
2026-02-04 22:22:40 +0530

    server edit

--- server/server.js
+++ server/server.js
@@ -1,21 +1,21 @@
-import express from 'express'
-import cors from 'cors'
-import 'dotenv/config'
-import { connect } from 'mongoose'
-import connectDB from './configs/db.js'
-import {inngest,functions} from './inngest/index.js'
-import {serve} from 'inngest/express'
-const app=express();
+import express from "express";
+import cors from "cors";
+import "dotenv/config";
+import connectDB from "./configs/db.js";
+import { inngest, functions } from "./inngest/index.js";
+import { serve } from "inngest/express";
 
-await connectDB()
+const app = express();
 
+await connectDB();
 
-app.use(express.json())
-app.use(cors())
+app.use(express.json());
+app.use(cors());
 
-app.get('/',(req,res)=> res.send('Server is running'))
-app.use('/api/inngest',serve({client:inngest,functions}))
+app.get("/", (req, res) => {
+  res.send("Server is running 🚀");
+});
 
-const PORT = process.env.PORT || 4000
+app.use("/api/inngest", serve({ client: inngest, functions }));
 
-app.listen(PORT,()=>console.log(`Server is running on port ${PORT}`))
+export default app; // 🔥 IMPORTANT
