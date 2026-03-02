0942b67e26d15832cd94c39ba3ec786f091183de
Amaidk7 <amaidkhan515@gmail.com>
2026-03-02 12:30:16 +0530

    restartserver

--- server/server.js
+++ server/server.js
@@ -1,21 +1,17 @@
 import express from "express";
 import cors from "cors";
-import "dotenv/config";
-import connectDB from "./configs/db.js";
-import { inngest, functions } from "./inngest/index.js";
-import { serve } from "inngest/express";
-
+import 'dotenv/config';
+ import connectDB from "./configs/db.js";
 const app = express();
 
 await connectDB();
-
 app.use(express.json());
 app.use(cors());
 
 app.get("/", (req, res) => {
-  res.send("Server is running 🚀");
+  res.send("Server is running");
 });
 
-app.use("/api/inngest", serve({ client: inngest, functions }));
+const PORT = process.env.PORT || 4000;
 
-export default app; // 🔥 IMPORTANT
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
