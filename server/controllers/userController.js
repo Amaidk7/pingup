4b328b1b3527013e5be598760944f2d2fcfe13f3
Amaidk7 <amaidkhan515@gmail.com>
2026-03-03 00:11:35 +0530

    somecontrollerfunc

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,4 +1,5 @@
 import imagekit from "../configs/imageKit.js";
+import Connection from "../models/Connection.js";
 import User from "../models/User.js";
 import fs from "fs";
 // Get User Data using userId
@@ -22,12 +23,14 @@ export const getUserData = async (req, res) => {
 export const updateUserData = async (req, res) => {
   try {
     const { userId } = req.auth();
-    const { username, full_name, bio, location } = req.body;
+    let { username, full_name, bio, location } = req.body;
     const tempUser = await User.findById(userId);
-
+    if (!tempUser) {
+      return res.json({ success: false, message: "User not found" });
+    }
     !username && (username = tempUser.username);
     if (tempUser.username !== username) {
-      const user = User.findOne({ username });
+      const user = await User.findOne({ username });
       if (user) {
         //we will not change the username if it is already taken
         username = tempUser.username;
@@ -161,3 +164,44 @@ export const unfollowUser = async (req, res) => {
     res.json({ success: false, message: error.message });
   }
 };
+
+
+
+//send connection request
+
+export const sendConnectionRequest = async (req, res) => {
+    try {
+        const {userId}=req.auth()
+        const {id}=req.body;
+        
+        // check if user has sent more than 20 connection requests in the last 24 hours
+        const last24Hours = new Date(Date.now() - 24*60*60*1000);
+        const connectionRequests=await Connection.find({from_user_id:userId,created_at:{$gt: last24Hours}})
+        if(connectionRequests.length>=20){
+            return res.json({success:false,message:'You have sent more than 20 connection requests in the last 24 hours. Please try again later.'})
+        }
+
+        //check if users are already connected
+        const connection = await Connection.findOne({
+            $or:[
+                {rom_user_id:userId,to_user_id:id},
+                {rom_user_id:id,to_user_id:userId},
+            ]
+        })
+
+        if(!connection){
+            await Connection.create({
+                from_user_id:userId,
+                to_user_id:id,
+            })
+            return res.json({success:true,message:'Connection request sent successfully'})
+        }else if(connection && connection.status === 'accepted'){
+            return res.json({success:false,message:'You are already connected with this user'})
+        }
+
+        return res.json({success:false,message:'Connection request already sent. Please wait for the user to accept your request.'})
+    } catch (error) {
+        console.log(error);
+        res.json({success:false,message:error.message})
+    }
+}
