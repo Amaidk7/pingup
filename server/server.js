4d10153ce7f2677624dcbd6b82f07284c93acf07
Amaidk7 <amaidkhan515@gmail.com>
2026-03-03 19:00:19 +0530

    postfunc

--- server/server.js
+++ server/server.js
@@ -9,6 +9,7 @@ import { serve } from "inngest/express";
 
 import { clerkMiddleware } from "@clerk/express";
 import userRouter from "./routes/userRoutes.js";
+import postRouter from "./routes/postRoutes.js";
 const app = express();
 
 await connectDB();
@@ -20,7 +21,7 @@ app.get("/", (req, res) => {
 });
 app.use("/api/inngest", serve({ client: inngest, functions }));
 app.use("/api/user", userRouter);
-
+app.use('/api/post',postRouter)
 const PORT = process.env.PORT || 4000;
 
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
