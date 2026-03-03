b4aa35f9085674301c283e7339c1a0eef45117ba
Amaidk7 <amaidkhan515@gmail.com>
2026-03-03 16:34:45 +0530

    emailserviceadded

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -205,3 +205,53 @@ export const sendConnectionRequest = async (req, res) => {
         res.json({success:false,message:error.message})
     }
 }
+
+
+//get user connections
+
+export const getUserConnections = async (req, res) => {
+    try {
+        const {userId}=req.auth()
+        const user =await User.findyById(userId).populate('connections followers following');
+        const connections = user.connections
+        const followers = user.followers
+        const following = user.following
+
+        const pendingConnections = (await Connection.find({to_user_id:userId,status:'pending'}).populate('from_user_id')).map((connection)=>connection.from_user_id)
+
+        res.json({success:true,connections,pendingConnections,followers,following})
+    } catch (error) {
+        console.log(error);
+        res.json({success:false,message:error.message})
+    }
+}
+
+
+//accept connection request
+
+export const acceptConnectionRequest = async (req, res) => {
+    try {
+        const {userId}=req.auth()
+        const {id}=req.body;
+        const connection = await Connection.findOne({from_user_id:id,to_user_id:userId})
+        if(!connection){
+            return res.json({success:false,message:'Connection request not found'})
+        }
+        const user = await User.findById(userId);
+        user.connections.push(id);
+        await user.save();
+
+        const toUser = await User.findById(userId);
+        toUser.connections.push(id);
+        await toUser.save();
+
+        connection.status = 'accepted';
+        await connection.save();
+
+        res.json({success:true,message:'Connection request accepted successfully'})
+
+    } catch (error) {
+        console.log(error);
+        res.json({success:false,message:error.message})
+    }
+}
--- server/inngest/index.js
+++ server/inngest/index.js
@@ -1,67 +1,128 @@
 import { Inngest } from "inngest";
 import User from "../models/User.js";
+import Connection from "../models/Connection.js";
+import sendEmail from "../configs/nodeMailer.js";
 
 // Create a client to send and receive events
 export const inngest = new Inngest({ id: "pingup-app" });
 
-
 // Inngest function to save user data to a database
 
 const syncUserCreation = inngest.createFunction(
-    {id:'sync-user-from-clerk'},
-    {event:'clerk/user.created'},
-    async ({event}) => {
-        const {id,first_name,last_name,email_addresses,image_url} = event.data;
-        let username = email_addresses[0].email.split('@')[0];
+  { id: "sync-user-from-clerk" },
+  { event: "clerk/user.created" },
+  async ({ event }) => {
+    const { id, first_name, last_name, email_addresses, image_url } =
+      event.data;
+    let username = email_addresses[0].email.split("@")[0];
 
-        //check availability of username
-        const user = await User.findOne({username})
+    //check availability of username
+    const user = await User.findOne({ username });
 
-        if(user){
-            username = username + Math.floor(Math.random()*10000);
-        }
-
-        const userData={
-            _id:id,
-            email:email_addresses[0].email_addresses,
-            full_name:first_name + ' ' + last_name,
-            username,
-            profile_picture:image_url
-        }
-        await User.create(userData);
+    if (user) {
+      username = username + Math.floor(Math.random() * 10000);
     }
-)
+
+    const userData = {
+      _id: id,
+      email: email_addresses[0].email_addresses,
+      full_name: first_name + " " + last_name,
+      username,
+      profile_picture: image_url,
+    };
+    await User.create(userData);
+  },
+);
 
 // Inngest function to update user data in database
 
 const syncUserUpdation = inngest.createFunction(
-    {id:'update-user-from-clerk'},
-    {event:'clerk/user.updated'},
-    async ({event}) => {
-        const {id,first_name,last_name,email_addresses,image_url} = event.data;
-        
-        const updatedUserData = {
-            email:email_addresses[0].email_addresses,
-            full_name:first_name + ' ' + last_name,
-            profile_picture:image_url
-        }
-        await User.findByIdAndUpdate(id,updatedUserData);
-    }
-)
+  { id: "update-user-from-clerk" },
+  { event: "clerk/user.updated" },
+  async ({ event }) => {
+    const { id, first_name, last_name, email_addresses, image_url } =
+      event.data;
+
+    const updatedUserData = {
+      email: email_addresses[0].email_addresses,
+      full_name: first_name + " " + last_name,
+      profile_picture: image_url,
+    };
+    await User.findByIdAndUpdate(id, updatedUserData);
+  },
+);
 
 // Inngest function to delete user from database
 
 const syncUserDeletion = inngest.createFunction(
-    {id:'delete-user-from-clerk'},
-    {event:'clerk/user.deleted'},
-    async ({event}) => {
-        const {id} = event.data;
-        await User.findByIdAndDelete(id);
-    }
-)
+  { id: "delete-user-from-clerk" },
+  { event: "clerk/user.deleted" },
+  async ({ event }) => {
+    const { id } = event.data;
+    await User.findByIdAndDelete(id);
+  },
+);
 
+//inngest function to send reminder when a new connection request is added
+const sendNewConnectionReminder = inngest.createFunction(
+  { id: "send-new-connection-request-reminder" },
+  { event: "app/connection-request" },
+  async ({ event, step }) => {
+    const { connectionId } = event.data;
+
+    await step.run("send-connection-request-mail", async () => {
+      const connection = await Connection.findById(connectionId)
+        .populate("from_user_id")
+        .populate("to_user_id");
+      const subject = "👋 New Connection Request";
+      const body = `<div style="font-family:Arial,sans-serif;padding:20px;">
+            <h2>Hi ${connection.to_user_id.full_name},<h2/>
+            <p>You have a new connection request from ${connection.from_user_id.full_name} - @${connection.from_user_id.username}</p>
+            <p> click <a href="${process.env.FRONTEND_URL}/connections" style="color:#10b981;">here</a> to accept or reject the request</p>
+            <br/>
+            <p>Thanks , <br/> PingUp - Stay Connected </p>
+            </div>`;
+
+      await sendEmail({
+        to: connection.to_user_id.email,
+        subject,
+        body,
+      });
+    });
+    const in24Hours = new Date(Date.now() + 24 * 60 * 60 * 1000);
+
+    await step.sleepUntil("wait-for-24-hours", in24Hours);
+    await step.run("send-connection-request-reminder", async () => {
+      const connection = await Connection.findById(connectionId).populate(
+        "from_user_id to_user_id",
+      );
+      if (connection.status === "accepted") {
+        return { message: "Already accepted" };
+      }
+      const subject = "👋 New Connection Request";
+      const body = `<div style="font-family:Arial,sans-serif;padding:20px;">
+            <h2>Hi ${connection.to_user_id.full_name},<h2/>
+            <p>You have a new connection request from ${connection.from_user_id.full_name} - @${connection.from_user_id.username}</p>
+            <p> click <a href="${process.env.FRONTEND_URL}/connections" style="color:#10b981;">here</a> to accept or reject the request</p>
+            <br/>
+            <p>Thanks , <br/> PingUp - Stay Connected </p>
+            </div>`;
+
+      await sendEmail({
+        to: connection.to_user_id.email,
+        subject,
+        body,
+      });
+
+      return { message: "Remainder sent" };
+    });
+  },
+);
 
 // Create an empty array where we'll export future Inngest functions
 export const functions = [
-    syncUserCreation,syncUserUpdation,syncUserDeletion
+  syncUserCreation,
+  syncUserUpdation,
+  syncUserDeletion,
+  sendNewConnectionReminder,
 ];
