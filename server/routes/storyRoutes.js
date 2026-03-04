09573dde13d48e80d4c334cd5977004cd30d575f
Amaidk7 <amaidkhan515@gmail.com>
2026-03-04 10:17:34 +0530

    messageroutesetup

--- server/routes/storyRoutes.js
+++ server/routes/storyRoutes.js
@@ -1,11 +1,13 @@
 import express from "express";
 import { upload } from "../configs/multer.js";
 import { protect } from "../middlewares/auth.js";
-import { addUserStory } from "../controllers/storyControllers.js";
+import { addUserStory, getStories } from "../controllers/storyControllers.js";
+
+
 
 const storyRouter = express.Router();
 
-storyRouter.post("/create", upload.single("media"), protect, addUserStory);
+storyRouter.post('/create',upload.single('media'),protect,addUserStory)
 
-storyRouter.get("/get", protect, getStories);
+storyRouter.get('/get',protect,getStories)
 export default storyRouter;
